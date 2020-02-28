3fe55613f685d744ca5fd5491440c1cbd06d9049
lem-onade <taelemansst@gmail.com>
2020-02-28 15:57:34 +0100

    get question dingen

--- MJ-MavaJinitests(Testr)_Lector/next.py
+++
@@ -1,1 +0,0 @@
-def get_next_quesion(name):
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os, git, subprocess
-from flask import Flask, request, json
+from flask import Flask, request, json, jsonify
 from flask_mysqldb import MySQL
 
 
@@ -7,7 +7,7 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['MYSQL_USER'] = 'dht'
 app.config['MYSQL_PASSWORD'] = 'mvghetdhtmvghetdht'
-app.config['MYSQL_DB'] = 'mj'
+app.config['MYSQL_DB'] = 'mj_testr'
 app.config['MYSQL_HOST'] = 'localhost'
 mysql = MySQL(app)
 
@@ -16,6 +16,30 @@ mysql = MySQL(app)
 def index():
     return "Welcome! zieke flask server hier gemaakt door the Didactical Aids Team"
 
+@app.route("/question/<user>/current")
+def current_question(user):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT pathToQuestion FROM answer WHERE repoName=%s and datetimeOfAnswer is null;", [str(user)])
+    res = cursor.fetchall()
+    cursor.close()
+
+    if len(res) == 0:
+        return jsonify({"result": "gebruiker heeft nog geen vraag gekregen"})
+
+    current = res[0][0]
+
+    return jsonify({"result": res[0][0]})
+
+@app.route("/question/<user>/next")
+def next_question(user):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT pathToQuestion FROM answer WHERE repoName=%s and datetimeOfAnswer is null;", [str(user)])
+    res = cursor.fetchall()
+    cursor.close()
+
+
+
+    return "success"
 
 @app.route("/hook", methods=['POST'])
 def hook():
