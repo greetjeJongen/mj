102c5d86e1b09da4ecc49b54d74a50764278561f
SwordleihsUcll <43514236+SwordleihsUcll@users.noreply.github.com>
2020-03-04 14:12:04 +0100

    script nu documentatie?

--- MJ-MavaJinitests(Testr)_Lector/testr.rb
+++ MJ-MavaJinitests(Testr)_Lector/testr.rb
@@ -8,19 +8,23 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: testr.rb [options]"
 
+  # f option, direct output to a file
   opts.on("-f", "--file", "output to file") do |f|
     options[:file] = f
   end
 
+  # r option, print output without visual formatting (for easier data manipulation)
   opts.on("-r", "--raw", "raw output") do |r|
     options[:raw] = r
   end
 end.parse!
 
+# API call to DHT mainframe
 uri = URI('http://server.arne.tech:83/status/everyone')
 response = Net::HTTP.get(uri)
 r = JSON.parse(response)
 
+# Check which repo name is the longest
 max_length = 0
 r.each do
   |student_info|
@@ -30,12 +34,14 @@ r.each do
 end
 max_length += 1
 
+# Build output string
 result = ''
 unless options[:raw]
   result += '%s' % 'repo'.ljust(max_length, ' ') + "│passed\n"
   result += '%s' % '────'.ljust(max_length, '─') + "├──────\n"
 end
 
+# Build line for each repository
 r.each do |student_info|
   result += '%s' % student_info['repoName'].ljust(max_length, ' ')
   passed_text = 'true'
@@ -47,9 +53,9 @@ r.each do |student_info|
   else
     result += "│%s\n" % passed_text.ljust(2, ' ')
   end
-
 end
 
+# Output generation
 if options[:file]
   File.open("output.txt", "w+") {|f| f.write(result)}
 else
